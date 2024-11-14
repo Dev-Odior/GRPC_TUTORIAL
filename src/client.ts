dda508969b1b572319b75ea1ca58c08f5bc03580
Dev-Odior <godspoweraino@gmail.com>
2024-11-14 07:36:44 +0100

    done with grpc

--- src/client.ts
+++ src/client.ts
@@ -2,6 +2,7 @@ import path from "path";
 
 import * as grpc from "@grpc/grpc-js";
 import * as protoLoader from "@grpc/proto-loader";
+import readline from "readline";
 
 import { ProtoGrpcType } from "./proto/random";
 
@@ -53,18 +54,53 @@ function onClientReady() {
   //   console.log("communication ended");
   // });
 
-  const stream = client.TodoList((err, res) => {
-    if (err) {
-      console.log(err);
-    }
+  // const stream = client.TodoList((err, res) => {
+  //   if (err) {
+  //     console.log(err);
+  //   }
 
-    console.log(res);
+  //   console.log(res);
+  // });
+
+  // stream.write({ todo: "walk the wife", status: "never" });
+  // stream.write({ todo: "walk the dog", status: "impossible" });
+  // stream.write({ todo: "Get a real job", status: "done" });
+  // stream.write({ todo: "Feed the cats", status: "never" });
+
+  // stream.end()
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
   });
 
-  stream.write({ todo: "walk the wife", status: "never" });
-  stream.write({ todo: "walk the dog", status: "impossible" });
-  stream.write({ todo: "Get a real job", status: "done" });
-  stream.write({ todo: "Feed the cats", status: "never" });
+  const username = process.argv[2];
 
-  stream.end()
+  if (!username) {
+    console.error(`No username, can't join chat`);
+    process.exit();
+  }
+
+  const metadata = new grpc.Metadata();
+  metadata.set("username", username);
+  const call = client.chat(metadata);
+
+  call.write({
+    message: "register",
+  });
+
+  call.on("data", (chunk) => {
+    console.log(`${chunk.username} ==> ${chunk.message}`);
+  });
+
+  rl.on("line", (line) => {
+    if (line === "quit") {
+      call.end();
+      return;
+    } else {
+      call.write({
+        message: line,
+      });
+    }
+  });
 }
--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import * as grpcProtoLoader from "@grpc/proto-loader";
 import { ProtoGrpcType } from "./proto/random";
 import { RandomHandlers } from "./proto/randomPackage/Random";
 import { TodoResponse } from "./proto/randomPackage/TodoResponse";
+import { ChatRequest } from "./proto/randomPackage/ChatRequest";
+import { ChatResponse } from "./proto/randomPackage/ChatResponse";
 
 const port = 8092;
 const PROTO_FILE = "./proto/random.proto";
@@ -36,6 +38,11 @@ const main = () => {
 };
 
 let todoList: TodoResponse = { todos: [] };
+const calObjByUsername = new Map<
+  string,
+  grpc.ServerDuplexStream<ChatRequest, ChatResponse>
+>();
+
 const getServer = () => {
   const server = new grpc.Server();
 
@@ -72,6 +79,36 @@ const getServer = () => {
         callback(null, {
           todos: todoList.todos,
         });
+      });
+    },
+
+    Chat: (call) => {
+      call.on("data", (req) => {
+        const username = call.metadata.get("username")[0] as string;
+        const message = req.message;
+        console.log(req);
+
+        for (let [user, usersCall] of calObjByUsername) {
+          if (username !== user) {
+            usersCall.write({ username, message });
+          }
+        }
+
+        if (calObjByUsername.get(username) === undefined) {
+          calObjByUsername.set(username, call);
+        }
+      });
+
+      call.on("end", () => {
+        const username = call.metadata.get("username")[0] as string;
+        calObjByUsername.delete(username);
+        console.log(`${username} is ending their chat session`);
+        call.write({
+          username: "Server",
+          message: `See you later ${username}`,
+        });
+
+        call.end();
       });
     },
   } as RandomHandlers);
